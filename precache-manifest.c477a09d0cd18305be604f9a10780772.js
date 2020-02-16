cf2c737e1d7353338b77a995d04b4381ac9d1a5d
iamsomraj <iamsomraj@gmail.com>
2020-02-16 18:38:53 +0530

    Updates

---
+++ precache-manifest.c477a09d0cd18305be604f9a10780772.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d6017bfacdc1f3d230df430c71c418",
+    "url": "/React-Burger-Builder/index.html"
+  },
+  {
+    "revision": "93a459d7b097d0ee0886",
+    "url": "/React-Burger-Builder/static/css/main.e9de1473.chunk.css"
+  },
+  {
+    "revision": "f4d37f459cd5b1a4a9f2",
+    "url": "/React-Burger-Builder/static/js/2.73f54dc8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Burger-Builder/static/js/2.73f54dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a459d7b097d0ee0886",
+    "url": "/React-Burger-Builder/static/js/main.1592c895.chunk.js"
+  },
+  {
+    "revision": "617f058d54cdb709d71a",
+    "url": "/React-Burger-Builder/static/js/runtime-main.39cd3a11.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/React-Burger-Builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
